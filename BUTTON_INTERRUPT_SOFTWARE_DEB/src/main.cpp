52ba1e7c8523ce32d4a8b3ade7bf392aee686301
volodymyr345 <maksymivvolodymyr089@gmail.com>
2026-08-19 11:18:06 +0300

    create project button interrupt timer debounce

---
+++ BUTTON_INTERRUPT_SOFTWARE_DEB/src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#define BUTTON_PIN 1
+
+
+
+const uint8_t debounce_ms = 50;
+
+
+//timer for interrupt on
+volatile uint16_t counter = 0;
+volatile uint64_t timeWhenFired; 
+
+volatile bool interruptFired = 0;
+
+
+void IRAM_ATTR ISR() {
+  timeWhenFired = millis();
+  counter++;
+  interruptFired = 1;
+}
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(BUTTON_PIN, ISR, FALLING);
+  Serial.begin(115200);
+}
+
+void loop() {
+  if(interruptFired && millis() - timeWhenFired > 50) {
+    attachInterrupt(BUTTON_PIN, ISR, FALLING);
+    interruptFired = !interruptFired;
+    Serial.println("How many times interrupt fired: ");
+    Serial.println(counter);
+    counter = 0;
+  } 
+  else if (interruptFired && millis() - timeWhenFired < 50) {
+    detachInterrupt(BUTTON_PIN);
+  }
+
+
+}
