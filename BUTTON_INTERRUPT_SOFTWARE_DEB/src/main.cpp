d09a692c1f664d7c8cb94f8dd0686389ca9787fa
volodymyr345 <maksymivvolodymyr089@gmail.com>
2026-08-20 11:36:09 +0300

    rework debounce state tracking using isrAttached flag

--- BUTTON_INTERRUPT_SOFTWARE_DEB/src/main.cpp
+++ BUTTON_INTERRUPT_SOFTWARE_DEB/src/main.cpp
@@ -2,22 +2,17 @@
 #define BUTTON_PIN 1
 
 //software timer debug
-
-
 const uint8_t debounce_ms = 50;
 
 
 //timer for interrupt on
 volatile uint16_t counter = 0;
 volatile uint64_t timeWhenFired; 
-
-volatile bool interruptFired = 0;
-
+bool isrAttached = 1;
 
 void IRAM_ATTR ISR() {
   timeWhenFired = millis();
   counter++;
-  interruptFired = 1;
 }
 
 void setup() {
@@ -27,14 +22,17 @@ void setup() {
 }
 
 void loop() {
-  if(interruptFired && millis() - timeWhenFired > 50) {
-    interruptFired = !interruptFired;
+  // if time is bigger than 50 and isrAttached = 0 then
+  if(millis() - timeWhenFired > debounce_ms && !isrAttached) { 
+    attachInterrupt(BUTTON_PIN, ISR, FALLING);
+    isrAttached = !isrAttached;
+  }
+  // if time is less than 50 and isrAttached = 1 then 
+  else if (millis() - timeWhenFired < debounce_ms && isrAttached) { 
     Serial.println("How many times interrupt fired: ");
     Serial.println(counter);
-    counter = 0;
-    attachInterrupt(BUTTON_PIN, ISR, FALLING);
-  } 
-  else if (interruptFired && millis() - timeWhenFired < 50) {
     detachInterrupt(BUTTON_PIN);
+    isrAttached = !isrAttached;
+    counter = 0;
   }
 }
