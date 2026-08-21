64287bbe8bacfe1c1f71e558805b4d539f7bb5a6
volodymyr345 <maksymivvolodymyr089@gmail.com>
2026-08-21 13:44:06 +0300

    implement debounce as state machine per task requirements

--- POLLINGDEBOUNCE_BUTTON/src/main.cpp
+++ POLLINGDEBOUNCE_BUTTON/src/main.cpp
@@ -1,37 +1,63 @@
 #include <Arduino.h>
 #define BUTTON_PIN 1
+#define LED_PIN 4
 
-const uint8_t debounce_ms = 10;
+const uint8_t debounce_ms = 8;
+const uint8_t poll_interval_ms = 10;
 unsigned long start_debounce = 0;
 
-
-
+unsigned long last_poll = 0;
 bool button_state = true;
 bool last_button_state = true;
-bool button_pushed = false;
+bool reading = true;
+bool led_state = false;
 
+enum DebounceState {
+  IDLE,
+  DEBOUNCING
+};
+
+enum ButtonState {
+  ON,
+  OFF
+};
+
+
+ButtonState state = OFF;
+DebounceState debState = IDLE;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
 }
 
 void loop() {
-  bool reading = digitalRead(BUTTON_PIN);
-  // if something has changed with reading or last_button_state and they are not equal to each other  then 
+  if(millis() - last_poll >= poll_interval_ms){
+    last_poll = millis();
+    reading = digitalRead(BUTTON_PIN);
+  }
+  
   if(reading != last_button_state) {
     start_debounce = millis();
+    debState = DEBOUNCING;
   }
-  if(millis() - start_debounce > debounce_ms) {
+  if(debState == DEBOUNCING && millis() - start_debounce > debounce_ms) {
     button_state = reading;
-    // if button is pressed (button_state is false) and button_pushed is false then 
-    if(!button_state && !button_pushed) { 
-      Serial.println("Button pushed");
-      button_pushed = !button_pushed;
-
-    }else if(button_state && button_pushed) { // if button is not pressed and button_pushed is true then 
-      button_pushed = false;
-      Serial.println("Button released");
+    debState = IDLE;
+    switch(state) {
+      case OFF:
+        if(!button_state) {
+          state = ON;
+          Serial.println("State is ON");
+        }
+        break;
+      case ON:
+        if(button_state) {
+          state = OFF;
+          Serial.println("State is OFF");
+        }
+        break;
     }
   }
   last_button_state = reading;
